86ca65fbd5c5a0e17ca24b92ea6b7c8662399d5e
Gabriele Moia <gabriele.moia@icloud.com>
2018-03-28 18:25:46 +0200

    Visualizzazione immagine completata

--- app/src/main/java/com/example/giulia/volleyobjectrequest/MainActivity.java
+++ app/src/main/java/com/example/giulia/volleyobjectrequest/MainActivity.java
@@ -1,74 +1,114 @@
 package com.example.giulia.volleyobjectrequest;
 
 import android.app.ProgressDialog;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    String url = "https://androidtutorialpoint.com/api/volleyJsonObject";
+    String url = "https://goo.gl/images/VJUoPQ";
+    private ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ProgressDialog dialog = new ProgressDialog(MainActivity.this);
-        dialog.setMessage("Caricamento...");
-        dialog.show();
+        Button button = (Button) findViewById(R.id.button);
+        final ImageView imageView = (ImageView) findViewById(R.id.iv_internal);
 
-        //Recupero stringa Json e lo visualizzo nella textView
-        /*JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                Log.d("Service", "Response:" + response.toString());
-                TextView details=(TextView) findViewById(R.id.details);
-                details.setText(response.toString());
-                dialog.hide();
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.d("Volley","Error", error.getMessage());
-                dialog.hide();
-            }
-        });*/
 
-        //Recupero stringa JSon, conversione in un oggetto e visualizzazione caratteristiche in textView
-        GsonRequest jsonObjectRequest = new GsonRequest(url, Telefono.class, null,
-                new Response.Listener<Telefono>() {
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+                dialog.setMessage("Caricamento...");
+                dialog.show();
+
+                ImageRequest imageRequest = new ImageRequest("https://cdn.pixabay.com/photo/2017/07/11/19/50/free-pictures-2494806_960_720.jpg", new Response.Listener<Bitmap>() {
                     @Override
-                    public void onResponse(Telefono response) {
-                        TextView screenSize = (TextView) findViewById(R.id.screenSize);
-                        TextView name =(TextView) findViewById(R.id.name);
-                        TextView operatingSystem=(TextView) findViewById(R.id.operatingSystem);
-                        screenSize.setText(response.getScreenSize());
-                        name.setText(response.getName());
-                        operatingSystem.setText(response.getOperatingSystem());
+                    public void onResponse(Bitmap response) {
+                        Uri uri = saveImageToInternalStorage(response);
+                        imageView.setImageURI(uri);
                         dialog.hide();
                     }
-
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                TextView details = (TextView) findViewById(R.id.screenSize);
-                details.setText(error.getMessage());
-                dialog.hide();
+                }, 483, 480, ImageView.ScaleType.CENTER_CROP, Bitmap.Config.RGB_565,
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                error.printStackTrace();
+                            }
+                        }
+                );
+                ServiceQueueSingleton.getInstance(getApplicationContext()).addToRequestQueue(imageRequest);
             }
         });
-        ServiceQueueSingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjectRequest);
+    }
+
+    protected Uri saveImageToInternalStorage(Bitmap bitmap) {
+        // Initialize ContextWrapper
+        ContextWrapper wrapper = new ContextWrapper(getApplicationContext());
+
+        // Initializing a new file
+        // The bellow line return a directory in internal storage
+        File file = wrapper.getDir("Images", MODE_PRIVATE);
+
+        // Create a file to save the image
+        file = new File(file, "UniqueFileName" + ".jpg");
+
+        try {
+            // Initialize a new OutputStream
+            OutputStream stream = null;
+
+            // If the output file exists, it can be replaced or appended to it
+            stream = new FileOutputStream(file);
+
+            // Compress the bitmap
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+
+            // Flushes the stream
+            stream.flush();
+
+            // Closes the stream
+            stream.close();
+
+        } catch (IOException e) // Catch the exception
+        {
+            e.printStackTrace();
+        }
+
+        // Parse the gallery image url to uri
+        Uri savedImageURI = Uri.parse(file.getAbsolutePath());
+
+        // Return the saved image Uri
+        return savedImageURI;
     }
 }
